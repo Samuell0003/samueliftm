a0aad8c449e4761a5629c79098aae9a574c37db7
Samuell0003 <2003samuellucas@gmail.com>
2022-09-11 00:13:53 -0300

    ex1

---
+++ src/main/java/com/example/samueliftm/SamueliftmApplication.java
@@ -0,0 +1,13 @@
+package com.example.samueliftm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamueliftmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamueliftmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samueliftm/SamueliftmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samueliftm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamueliftmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
